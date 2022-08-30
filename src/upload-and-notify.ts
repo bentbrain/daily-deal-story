d30325e4b76471b08ab18a10e16f709c8e6029bc
bentbrain <liamtc@icloud.com>
2022-08-31 09:41:43 +1000

    mac 12

---
+++ src/upload-and-notify.ts
@@ -0,0 +1,34 @@
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import Slackbot from 'slackbot';
+import xns from 'xns';
+
+dotenv.config();
+
+xns(async () => {
+	const s3 = new AWS.S3({
+		accessKeyId: process.env.S3_ACCESS_KEY,
+		secretAccessKey: process.env.S3_SECRET_KEY,
+	});
+	const result = await new Promise<AWS.S3.ManagedUpload.SendData>(
+		(resolve, reject) => {
+			s3.upload(
+				{
+					Bucket: '',
+					Key: `daily-deal-${Date.now()}.mp4`,
+					Body: fs.readFileSync(path.join(__dirname, '..', 'out.mp4')),
+					ACL: 'public-read',
+				},
+				(err, data) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(data);
+					}
+				}
+			);
+		}
+	);
+});
